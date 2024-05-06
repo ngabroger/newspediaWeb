b87f680b26b4b005867944bb8873d6bcfb745e02
Roger Simanjuntak <arbination13@gmail.com>
2024-05-06 18:22:26 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
   content: ["app/src/**/*.{html,js}",
   "./node_modules/flowbite/**/*.js"
-
   ],
-=======
-  content: ["app/src/**/*.{html,js}"], // Daftar file yang akan diproses oleh Tailwind
->>>>>>> e225f3b70ed28e153d487009b78f7a2c9fbb0490
   theme: {
     extend: {
       colors: {
