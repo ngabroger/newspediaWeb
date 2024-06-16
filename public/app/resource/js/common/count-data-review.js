a1c7bd92f6bf9078e84637e34e925713647cf673
DaffaPutraS <daffap434@gmail.com>
2024-06-16 17:18:03 +0700

    update card review

---
+++ public/app/resource/js/common/count-data-review.js
@@ -0,0 +1,13 @@
+import { db } from '../configfb.js';
+import { ref, onValue } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js';
+
+const reviewCountSpan = document.getElementById('reviewCount');
+
+const reviewRef = ref(db, 'Review');
+
+// Fetch the data from Firebase and update the count
+onValue(reviewRef, (snapshot) => {
+    const reviewData = snapshot.val();
+    const count = reviewData ? Object.keys(reviewData).length : 0;
+    reviewCountSpan.textContent = count;
+});
